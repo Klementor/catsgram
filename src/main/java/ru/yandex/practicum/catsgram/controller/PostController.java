96e352720ab661872724e058ac4c0000ec8c8211
Klementor <mihailkrylov8189@gmail.com>
2022-09-22 19:52:47 +0300

    feature UserController

--- src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
@@ -1,7 +1,5 @@
 package ru.yandex.practicum.catsgram.controller;
 
-import org.slf4j.LoggerFactory;
-import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,9 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import ru.yandex.practicum.catsgram.model.Post;
 import ru.yandex.practicum.catsgram.service.PostService;
 
-import java.util.ArrayList;
 import java.util.List;
-
 
 @RestController
 public class PostController {
