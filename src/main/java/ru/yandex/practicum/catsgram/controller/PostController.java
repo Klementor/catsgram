f1f571a6206c46bdf9a962efa6ca669214e6398f
Klementor <mihailkrylov8189@gmail.com>
2022-09-23 18:31:56 +0300

    feature findPostById && findUserByEmail

--- src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
@@ -18,8 +18,21 @@ public class PostController {
     }
 
     @GetMapping("/posts")
-    public List<Post> findAll() {
-        return postService.findAll();
+    public List<Post> findAll(
+            @RequestParam(value = "page", defaultValue = "0", required = false) Integer page,
+            @RequestParam(value = "size", defaultValue = "10", required = false) Integer size,
+            @RequestParam(value = "sort", defaultValue = "desc", required = false) String sort) {
+
+
+        if(!(sort.equals("asc") || sort.equals("desc"))){
+            throw new IllegalArgumentException();
+        }
+        if(page < 0 || size <= 0){
+            throw new IllegalArgumentException();
+        }
+
+        Integer from = page * size;
+        return postService.findAll(size, from, sort);
     }
 
     @PostMapping(value = "/post")
