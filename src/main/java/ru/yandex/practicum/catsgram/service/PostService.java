0daefc534b93ac4bf63d535abe54fb0c505ad478
Klementor <mihailkrylov8189@gmail.com>
2022-09-23 16:24:50 +0300

    feature findPostById && findUserByEmail

--- src/main/java/ru/yandex/practicum/catsgram/service/PostService.java
+++ src/main/java/ru/yandex/practicum/catsgram/service/PostService.java
@@ -1,23 +1,44 @@
 package ru.yandex.practicum.catsgram.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
 import ru.yandex.practicum.catsgram.exception.PostNotFoundException;
+import ru.yandex.practicum.catsgram.exception.UserNotFoundException;
 import ru.yandex.practicum.catsgram.model.Post;
+import ru.yandex.practicum.catsgram.model.User;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class PostService {
+    private final UserService userService;
     private final List<Post> posts = new ArrayList<>();
+
+    private static Integer globalId = 0;
+
+    @Autowired
+    public PostService(UserService userService) {
+        this.userService = userService;
+    }
 
     public List<Post> findAll() {
         return posts;
     }
 
+    private static Integer getNextId(){
+        return globalId++;
+    }
+
     public Post create(Post post) {
+        User postAuthor = userService.findUserByEmail(post.getAuthor());
+        if (postAuthor == null) {
+            throw new UserNotFoundException(String.format(
+                    "Пользователь %s не найден",
+                    post.getAuthor()));
+        }
+
+        post.setId(getNextId());
         posts.add(post);
         return post;
     }
