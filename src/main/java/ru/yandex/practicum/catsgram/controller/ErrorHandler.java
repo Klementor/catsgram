4a2039de95bc510bc8337736f2b115d9d8e59ec8
Klementor <mihailkrylov8189@gmail.com>
2022-10-13 10:39:28 +0300

    fix findAll

--- src/main/java/ru/yandex/practicum/catsgram/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/ErrorHandler.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.catsgram.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.yandex.practicum.catsgram.exception.*;
 import ru.yandex.practicum.catsgram.model.ErrorResponse;
